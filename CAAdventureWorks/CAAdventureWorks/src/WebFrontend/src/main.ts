86702f76896290d98cc7a8b3a9c802f62c8fb31f
Lê Tiến Dũng <141888173+Yuryii@users.noreply.github.com>
2026-04-21 04:21:19 +0700

    .

--- CAAdventureWorks/CAAdventureWorks/src/WebFrontend/src/main.ts
+++ CAAdventureWorks/CAAdventureWorks/src/WebFrontend/src/main.ts
@@ -1,11 +1,5 @@
-/// <reference types="@angular/localize" />
 import { bootstrapApplication } from '@angular/platform-browser';
-import { inject } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-bootstrapApplication(AppComponent, appConfig).then(() => {
-  const oidcSecurityService = inject(OidcSecurityService);
-  oidcSecurityService.checkAuth().subscribe();
-});
+bootstrapApplication(AppComponent, appConfig);
